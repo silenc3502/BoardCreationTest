986001d9994c466280b75ce45c5b826c97fa015f
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-02 15:50:25 +0900

    feat: Dice 불변 객체 완료 [DICE-GAME-1]

---
+++ dicegame/src/player/Dice.java
@@ -0,0 +1,15 @@
+package player;
+
+import utility.CustomRandom;
+
+public class Dice {
+
+    final private int MIN = 1;
+    final private int MAX = 6;
+    final private int diceNumber;
+
+    public Dice () {
+        this.diceNumber =
+                CustomRandom.createCustomRandom(MIN, MAX);
+    }
+}
